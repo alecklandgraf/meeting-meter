4f978dde627062cf1990bd20f0f28d6c67fe2326
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2012-12-09 23:15:27 -0800

    refactor into command line argv friendly format

--- meeting_meter.py
+++ meeting_meter.py
@@ -1,19 +1,33 @@
 #!/usr/local/bin/python
-
+""" meeting_meter.py
+    Keeps track of total cost of the meeting as it goes.
+    TODO: 1. add logger to keep notes.
+          2. add ability to end meeting without ^C
+          3. add ability to update nominal salary
+"""
+import sys
 import time
+import datetime
 
 
-def main():
-    number_attendees = 7
-    nomiinal_salary = .75
-    update_interval_seconds = 60
+def run_meter(number_attendees, nominal_salary=.75, update_interval_seconds=60):
     total = 0
-
+    print "Meeting started at %s" % (datetime.datetime.now(), )
     while True:
-        total += number_attendees * nomiinal_salary * update_interval_seconds / 60
-        print "total cost of meeting: $%.2f" % (total, )
         time.sleep(update_interval_seconds)
+        total += number_attendees * nominal_salary * update_interval_seconds / 60
+        print "total cost of meeting: $%.2f" % (total, )
+
+
+def main(argv):
+    if len(argv) > 0:
+        number_attendees = int(argv[0])
+        run_meter(number_attendees)
+    else:
+        print "Missing number of attendees"
+        print "Usage: meeting_meter.py [number of attendees]"
+        print "Example: meeting_meter.py 7"
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
