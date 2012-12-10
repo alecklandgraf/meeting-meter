17e27774a2943b3f42a2807dc7c69556f6f4b1ce
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2012-12-09 22:04:11 -0800

    adding meeting interval variable

--- meeting_meter.py
+++ meeting_meter.py
@@ -6,12 +6,13 @@ import time
 def main():
     number_attendees = 7
     nomiinal_salary = .75
+    update_interval = 60
     total = 0
 
     while True:
-        total += number_attendees * nomiinal_salary
+        total += number_attendees * nomiinal_salary * update_interval / 60
         print "total cost of meeting: $%.2f" % (total, )
-        time.sleep(5)
+        time.sleep(update_interval)
 
 
 if __name__ == "__main__":
