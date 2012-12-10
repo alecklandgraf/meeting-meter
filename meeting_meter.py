cd7f586d7d160521ea83ce182de58dd88913fab3
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2012-12-09 22:35:49 -0800

    adds interval

--- meeting_meter.py
+++ meeting_meter.py
@@ -6,13 +6,13 @@ import time
 def main():
     number_attendees = 7
     nomiinal_salary = .75
-    update_interval = 60
+    update_interval_seconds = 60
     total = 0
 
     while True:
-        total += number_attendees * nomiinal_salary * update_interval / 60
+        total += number_attendees * nomiinal_salary * update_interval_seconds / 60
         print "total cost of meeting: $%.2f" % (total, )
-        time.sleep(update_interval)
+        time.sleep(update_interval_seconds)
 
 
 if __name__ == "__main__":
