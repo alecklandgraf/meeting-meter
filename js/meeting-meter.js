ce5384285d4ca2b8ed99b54399223ce1cbe3d379
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-03-01 00:14:17 -0800

    getting there

--- js/meeting-meter.js
+++ js/meeting-meter.js
@@ -1,2 +1,18 @@
 // meeting-meter.js
 var MeetingMeter = MeetingMeter || {};
+
+MeetingMeter.options = MeetingMeter.options || {};
+MeetingMeter.options.update_interval_seconds = 60;
+MeetingMeter.options.nominal_salary = 0.75;
+
+MeetingMeter.start_meeting = function () {
+	var selected_number_of_people = $(".pick_how_many_people").val();
+	var minutely_cost = selected_number_of_people * MeetingMeter.options.nominal_salary * MeetingMeter.options.update_interval_seconds / 60;
+	$("#cost_per_minute").html(minutely_cost.toFixed(2).toString());
+	$("#cost_per_hour").html((minutely_cost * 60).toFixed(2).toString());
+
+};
+
+$("#start_meeting_button").live("click", function () {
+	MeetingMeter.start_meeting();
+});
