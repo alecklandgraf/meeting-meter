d60eca86241a623538df04850c6e5a7a9216efbc
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-03-01 01:03:28 -0800

    almost done

--- js/meeting-meter.js
+++ js/meeting-meter.js
@@ -8,27 +8,41 @@ MeetingMeter.options = MeetingMeter.options || {
 
 MeetingMeter.timerId = MeetingMeter.timerId || 0;
 MeetingMeter.duration = MeetingMeter.duration || 0;
+MeetingMeter.initialStart = true;
+MeetingMeter.meetingStarted = false;
 
 MeetingMeter.start_meeting = function () {
-	var selected_number_of_people = $(".pick_how_many_people").val();
-	var minutely_cost = selected_number_of_people * MeetingMeter.options.nominal_salary * MeetingMeter.options.update_interval_seconds / 60;
-	MeetingMeter.duration = 0;
-	MeetingMeter.timerId = setInterval(function() { MeetingMeter.timer(minutely_cost);},1000);
-	var d = new Date();
-	var t = d.toLocaleTimeString();
+	if (!MeetingMeter.meetingStarted) {
+		MeetingMeter.meetingStarted = true;
+		var selected_number_of_people = $(".pick_how_many_people").val();
+		var minutely_cost = selected_number_of_people * MeetingMeter.options.nominal_salary * MeetingMeter.options.update_interval_seconds / 60;
+		MeetingMeter.timerId = setInterval(function() { MeetingMeter.timer(minutely_cost);},1000);
+		if (MeetingMeter.initialStart) {
+			var d = new Date();
+			var t = d.toLocaleTimeString();
+			$("#meeting_start_time").html(t);
+		}
 
-	$("#meeting_start_time").html(t);
-	$("#cost_per_minute").html("$" + minutely_cost.toFixed(2).toString());
-	$("#cost_per_hour").html("$" + (minutely_cost * 60).toFixed(2).toString());
-
-
+		
+		$("#cost_per_minute").html("$" + minutely_cost.toFixed(2).toString());
+		$("#cost_per_hour").html("$" + (minutely_cost * 60).toFixed(2).toString());
+	}
 };
 
 MeetingMeter.pause_meeting = function () {
+	MeetingMeter.initialStart = false;
+	MeetingMeter.meetingStarted = false;
 	clearInterval(MeetingMeter.timerId);
 };
 
 MeetingMeter.reset_meeting = function () {
+	MeetingMeter.duration = 0;
+	MeetingMeter.initialStart = true;
+	MeetingMeter.meetingStarted = false;
+	$("#meeting_start_time").html("");
+	$("#cost_per_minute").html("");
+	$("#cost_per_hour").html("");
+	$("#current_cost").html("");
 	clearInterval(MeetingMeter.timerId);
 };
 
@@ -49,5 +63,4 @@ MeetingMeter._event_bindings = function () {
 MeetingMeter.timer = function (cost) {
 	MeetingMeter.duration += 1;
 	$("#current_cost").html("$" + (MeetingMeter.duration * cost).toFixed(2));
-
 };
